d83a21a16b106acc0e880637f0946095650eb351
el mariachi <tomekbajorek@MacBook-Air-Tomek.local>
2015-06-10 18:40:58 +0200

    Initial Commit

---
+++ UserActivity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserActivity
+//
+//  Created by el mariachi on 10/06/15.
+//  Copyright (c) 2015 bitelz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserActivity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UserActivity
+//
+//  Created by el mariachi on 10/06/15.
+//  Copyright (c) 2015 bitelz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
